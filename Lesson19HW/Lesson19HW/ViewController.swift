1f8cc1b560fddf4557969f89e8ea461411f82b0e
pavelbraun333 <pavelbraun333@gmail.com>
2024-07-17 16:39:12 +0300

    project added

---
+++ Lesson19HW/Lesson19HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson19HW
+//
+//  Created by Pavel on 17.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
